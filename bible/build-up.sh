33b8f9fb30569c8e42e81aefd14d3bfdb9d01355
Jaewon Kim <epiloum@gmail.com>
2021-10-26 22:54:41 +0900

    fix config command

--- bible/build-up.sh
+++ bible/build-up.sh
@@ -11,7 +11,7 @@ echo "---------------------------"
 echo "    Docker Stack Deploy"
 echo "==========================="
 sleep 3
-docker-compose config -f docker-compose.yml > docker-compose-out.yml
+docker-compose -f docker-compose.yml config > docker-compose-out.yml
 docker stack deploy -c docker-compose-out.yml bible
 rm docker-compose-out.yml
 
