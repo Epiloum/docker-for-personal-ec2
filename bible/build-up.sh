03b96811039e480f1adc4070751426f7a7771464
Jaewon Kim <epiloum@gmail.com>
2021-10-26 22:00:28 +0900

    add sleep commands

--- bible/build-up.sh
+++ bible/build-up.sh
@@ -2,14 +2,17 @@
 
 # build
 docker-compose build --no-cache
+sleep 3
 
 # deploy by temporary configuration file
 docker-compose config -f docker-compose.yml > docker-compose-out.yml
 docker stack deploy -c docker-compose-out.yml bible
 rm docker-compose-out.yml
+sleep 3
 
 # up
 docker-compose up -d
+sleep 3
 
 # composer install
 docker exec $(docker ps -aqf "name=app_dev") composer install
