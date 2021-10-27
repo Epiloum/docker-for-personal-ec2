674744e70304995a817f37040e539636b428792f
epiloum <epiloum@gmail.com>
2021-10-27 16:34:45 +0000

    many changes

--- bible/build-up.sh
+++ bible/build-up.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-# build
+# swarm mode off
+echo "---------------------------"
+echo "      Docker Swarm Off"
+echo "==========================="
+docker swarm leave -f
+
+# docker compose build
 echo "---------------------------"
 echo "   Docker-compose Build"
 echo "==========================="
+sleep 3
+docker image prune -f
+sleep 3
 docker-compose build --no-cache
+
+# swarm mode on
+echo "---------------------------"
+echo "      Docker Swarm On"
+echo "==========================="
+sleep 3
+docker swarm init
 
 # deploy by temporary configuration file
 echo "---------------------------"
@@ -15,13 +31,6 @@ docker-compose -f docker-compose.yml config > docker-compose-out.yml
 docker stack deploy -c docker-compose-out.yml bible
 rm docker-compose-out.yml
 
-# up
-echo "---------------------------"
-echo "     Docker-compose Up"
-echo "==========================="
-sleep 3
-docker-compose up -d
-
 # composer install
 echo "---------------------------"
 echo "     Composer Install"
@@ -30,16 +39,7 @@ sleep 3
 docker exec $(docker ps -aqf "name=app_dev") composer install
 docker exec $(docker ps -aqf "name=app_dev") chmod -R +x /var/www/vendor
 docker exec $(docker ps -aqf "name=app_dev") chmod -R 777 /var/www/storage
-docker exec $(docker ps -aqf "name=app_dev") php artisan key:generate
-docker exec $(docker ps -aqf "name=app_dev") php artisan passport:keys
-docker exec $(docker ps -aqf "name=app_dev") php artisan storage:link
 
 docker exec $(docker ps -aqf "name=app_prod") composer install
 docker exec $(docker ps -aqf "name=app_prod") chmod -R +x /var/www/vendor
 docker exec $(docker ps -aqf "name=app_prod") chmod -R 777 /var/www/storage
-docker exec $(docker ps -aqf "name=app_prod") php artisan key:generate
-docker exec $(docker ps -aqf "name=app_prod") php artisan passport:keys
-docker exec $(docker ps -aqf "name=app_prod") php artisan storage:link
-
-
-
