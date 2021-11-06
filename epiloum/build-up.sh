5617f429ca0953ba1757d80d61839d9ea62f293a
Jaewon Kim <epiloum@gmail.com>
2021-11-07 02:04:48 +0900

    feat: remove sleep and add system prune

--- epiloum/build-up.sh
+++ epiloum/build-up.sh
@@ -10,25 +10,21 @@ docker swarm leave -f
 echo "---------------------------"
 echo "   Docker-compose Build"
 echo "==========================="
-sleep 3
 docker image prune -f
-sleep 3
+docker system prune -f
 docker-compose pull
-sleep 3
 docker-compose build --no-cache
 
 # swarm mode on
 echo "---------------------------"
 echo "      Docker Swarm On"
 echo "==========================="
-sleep 3
 docker swarm init
 
 # deploy by temporary configuration file
 echo "---------------------------"
 echo "    Docker Stack Deploy"
 echo "==========================="
-sleep 3
 docker-compose -f docker-compose.yml config > docker-compose-out.yml
 docker stack deploy -c docker-compose-out.yml epiloum
 rm docker-compose-out.yml
